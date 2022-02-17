0e831798a7a4fc283e96280c5c044fad5bd9b479
hestiawong <95612214+hestiawong@users.noreply.github.com>
2022-02-17 15:26:46 +0800

    hi

---
+++ app/src/main/java/com/example/fyp_app/ui/home/HomeFragment.java
@@ -0,0 +1,50 @@
+package com.example.fyp_app.ui.home;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.fyp_app.EntryChoiceActivity;
+import com.example.fyp_app.R;
+
+public class HomeFragment extends Fragment {
+    ImageView yogaArms;
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        yogaArms = root.findViewById(R.id.yogaArms);
+        /*final TextView textView = root.findViewById(R.id.text_home);
+        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+        yogaArms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), EntryChoiceActivity.class));
+            }
+        });
+        return root;
+
+
+
+    }
+    
+
+}
